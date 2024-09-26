94e43a89be7dc27f2b254ca83dfdee9c3c7a469f
M-Ali-Haider <mah01134212100@gmail.com>
2024-09-26 19:30:02 +0500

    .........

--- src/components/Features/card.jsx
+++ src/components/Features/card.jsx
@@ -3,9 +3,6 @@ const FeatureCard = ({ item }) => {
     <>
       <div className="p-6 rounded-xl bg-[#101010]">
         {item.svgComp}
-        {/* <div className="my-4 font-polySans text-[20px] leading-[28px] text-white">
-          {item.title}
-        </div> */}
         <div className="mt-[30px] text-xl leading-[30px] text-[#898A96]">
           {item.desc}
         </div>
