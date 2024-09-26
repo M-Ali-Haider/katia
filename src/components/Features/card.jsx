9a4137a4b9ed72724c70e8a423c0a9c34bee465f
M-Ali-Haider <mah01134212100@gmail.com>
2024-09-26 19:10:18 +0500

    ......

--- src/components/Features/card.jsx
+++ src/components/Features/card.jsx
@@ -6,7 +6,9 @@ const FeatureCard = ({ item }) => {
         {/* <div className="my-4 font-polySans text-[20px] leading-[28px] text-white">
           {item.title}
         </div> */}
-        <div className="text-base text-[#898A96]">{item.desc}</div>
+        <div className="mt-[30px] text-xl leading-[30px] text-[#898A96]">
+          {item.desc}
+        </div>
       </div>
     </>
   );
