4f23b1671f8c6bed6776acbe7869b4fcdffe26ec
M-Ali-Haider <mah01134212100@gmail.com>
2024-10-08 23:18:55 +0500

    added purple ring to input fields

--- src/components/Auth/inputfield.jsx
+++ src/components/Auth/inputfield.jsx
@@ -10,7 +10,8 @@ const InputField = ({ type, placeholder, label }) => {
       <input
         type={type}
         placeholder={placeholder}
-        className="w-full 2xl:text-base text-sm placeholder:text-[#98A2B3] rounded-lg bg-[#25262D] border-none py-3 2xl:py-4 px-[14px]"
+        className="w-full 2xl:text-base text-sm placeholder:text-[#98A2B3] rounded-lg bg-[#25262D] border-none py-3 2xl:py-4 px-[14px] 
+        focus:outline-none focus:ring-[1px] focus:ring-[#9773FF]"
       />
     </div>
   );
