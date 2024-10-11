4d68a3cedecf2cadb654a1fd1c639511110c537e
M-Ali-Haider <mah01134212100@gmail.com>
2024-10-11 16:13:42 +0500

    ........

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -14,11 +14,11 @@ const Sidebar = () => {
         isSidebarOpen
           ? `left-0 sm:w-[315px] pointer-events-auto`
           : `-left-[315px] sm:w-0 pointer-events-none`
-      } h-screen max-h-screen overflow-x-hidden flex 
+      } h-svh sm:h-screen max-h-svh sm:max-h-screen overflow-x-hidden flex 
       duration-500 ease-custom-ease transition-all
       fixed top-0 right-0 bottom-0 z-10 sm:static`}
     >
-      <div className="w-[315px] px-5 bg-[#0E0F21] border-r border-[#353741] h-screen max-h-screen flex flex-col">
+      <div className="w-[315px] px-5 bg-[#0E0F21] border-r border-[#353741] h-svh sm:h-screen max-h-svh sm:max-h-screen flex flex-col">
         <ProfileDetails className={"mt-10"} />
         <AIKatiaPlusButton className={"mt-[30px]"} />
         <History />
