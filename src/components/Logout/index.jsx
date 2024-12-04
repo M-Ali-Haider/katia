4b6e527c7b3b5238f0b836449acf5a100674dadf
M-Ali-Haider <mah01134212100@gmail.com>
2024-12-04 17:27:28 +0500

    ........

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -1,30 +1,41 @@
 "use client";
 import { logout, logoutDestroySession } from "@/actions/actions";
 import { removeFromLocalStorage } from "@/store/pfp";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 import { useDispatch } from "react-redux";
 import { toast } from "sonner";
+import LoadingRing from "../LoadingRing";
 
 export function Logout({ className }) {
   const dispatch = useDispatch();
   const [state, formAction] = useFormState(logout, undefined);
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     if (state?.success) {
-      toast.info(state.success);
       dispatch(removeFromLocalStorage());
       logoutDestroySession();
     }
     if (state?.error) {
       toast.error(state.error);
+      setLoading(false);
     }
   }, [state, dispatch]);
   return (
     <form action={formAction}>
       <button
-        className={`bg-[#F5F3C2] active:scale-95 transition duration-100 py-3 px-8 rounded-lg text-black ${className}`}
+        onClick={() => setLoading(true)}
+        className={`bg-[#F5F3C2] active:scale-95 flex items-center justify-center transition duration-100 py-3 px-8 rounded-lg text-black ${className}`}
       >
-        Logout
+        {isLoading ? (
+          <LoadingRing
+            width={"19.6px"}
+            height={"19.6px"}
+            colors={["#000000"]}
+          />
+        ) : (
+          <>Logout</>
+        )}
       </button>
     </form>
   );
